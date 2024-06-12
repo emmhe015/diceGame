40876857dcabe22da8ce90d29edd02bee67def81
Emma Henriksson <emmaha.eh@gmail.com>
2024-06-12 08:28:59 +0000

    Add if else if statements to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,11 @@
-function rollDice(player)
+function rollDice(player) {
 var randomNumber = Math.floor(Math.random() * 6) +1;
 var randomDiceImage = "dice" + randomNumber + ".png";
 var randomImageSource = "assets/images/" + randomDiceImage;
 
-
-
-if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML = "Player 1 wins!";
+if (player === 1) {
+    document.querySelector(".img1").setAttribute("src", randomImageSource);
+} else if (player === 2) {
+    document.querySelector(".img2").setAttribute("src", randomImageSource);
 }
-else if (randomNumber2 > randomNumber1) {
-    document.querySelector("h1").innerHTML = "Player 2 wins!";
-}
-else {
-    document.querySelector("h1").innerHTML = "Draw!";
 }
