3082855b1bfad63c98dd0a97255b3a673b01b20b
Emma Henriksson <emmaha.eh@gmail.com>
2024-06-12 08:25:18 +0000

    Add function rollDice to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,9 @@
-var randomNumber1 = Math.floor(Math.random() * 6) +1;
-
-var randomDiceImage = "dice" + randomNumber1 + ".png";
-
+function rollDice(player)
+var randomNumber = Math.floor(Math.random() * 6) +1;
+var randomDiceImage = "dice" + randomNumber + ".png";
 var randomImageSource = "assets/images/" + randomDiceImage;
-var image1 = document.querySelectorAll("img")[0];
-image1.setAttribute("src", randomImageSource);
 
-var randomNumber2 = Math.floor(Math.random() * 6) +1;
-var randomImageSource2 = "assets/images/dice" + randomNumber2 + ".png";
-document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
 
 if (randomNumber1 > randomNumber2) {
     document.querySelector("h1").innerHTML = "Player 1 wins!";
