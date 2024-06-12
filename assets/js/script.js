d74decc7637f9a51b368593b6af931bbd73cce2d
Emma Henriksson <emmaha.eh@gmail.com>
2024-06-12 08:45:26 +0000

    Add score variables to if statement in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,8 +8,12 @@ function rollDice(player) {
 
     if (player === 1) {
         document.querySelector(".img1").setAttribute("src", randomImageSource);
+        score1 += randomNumber;
+        document.getElementById("score1").textContent = score1;
     } else if (player === 2) {
         document.querySelector(".img2").setAttribute("src", randomImageSource);
+        score2 += randomNumber;
+        document.getElementById("score2").textContent = score2;
     }
 }
 document.getElementById("rollPlayer1").addEventListener("click", function() {
